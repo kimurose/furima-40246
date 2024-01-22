bcc8c67cc341099d12ef6e1cb8300b19affa434c
kimurose <154412065+kimurose@users.noreply.github.com>
2024-01-22 16:20:30 +0900

    Rubocopの実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name             {Faker::Name.initials(number: 2)}
-    first_name            {Faker::Name.initials(number: 2)}
-    last_name_kana        {Faker::Name.initials(number: 2)}
-    first_name_kana       {Faker::Name.initials(number: 2)}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { Faker::Name.initials(number: 2) }
+    first_name            { Faker::Name.initials(number: 2) }
+    last_name_kana        { Faker::Name.initials(number: 2) }
+    first_name_kana       { Faker::Name.initials(number: 2) }
     date                  { Faker::Date.between(from: Date.new(1930, 1, 1), to: Date.today) }
   end
 end
