b8cd2a0f9ed34855f54daf5e2a48f60859938af3
kimurose <154412065+kimurose@users.noreply.github.com>
2024-01-22 12:31:53 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :items
   has_many :orders
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :date, presence: true
 end
