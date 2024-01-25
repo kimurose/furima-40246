2df8586f8f1465e973cb2c9d2d1c03df9bd2b30a
kimurose <154412065+kimurose@users.noreply.github.com>
2024-01-25 13:14:08 +0900

    単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    image { Rack::Test::UploadedFile.new(Rails.root.join('app', 'assets', 'images', 'comment.png'), 'image/png') }
+    product_name     { '商品名' }
+    explanation      { '商品の説明' }
+    category_id      { 2 }
+    condition_id     { 2 }
+    delivery_cost_id { 2 }
+    prefecture_id    { 2 }
+    shipping_day_id  { 2 }
+    price            { 2000 }
   end
 end
